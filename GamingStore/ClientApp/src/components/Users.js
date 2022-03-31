d2323487d6b94c0143295a59a9cfb0c74773ccce
Vitalik Knysh <knish.vital@gmail.com>
2022-03-31 21:15:22 +0300

    Edit Users and UserRow components

--- GamingStore/ClientApp/src/components/Users.js
+++ GamingStore/ClientApp/src/components/Users.js
@@ -8,14 +8,13 @@ export class Users extends Component {
         super(props);
 
         this.state = {
-            users: [], 
+            users: [],
             role: sessionStorage.getItem('role'),
             token: sessionStorage.getItem('token')
         };
 
         this.getUsers = this.getUsers.bind(this);
         this.showUserInfo = this.showUserInfo.bind(this);
-        this.showUserInfoForAdmin = this.showUserInfoForAdmin.bind(this);
     }
 
     async componentDidMount() {
@@ -37,25 +36,8 @@ export class Users extends Component {
             .catch(error => console.log(error.message));
     }
 
-    updateUser(id) {
-        window.location.href = `/users/edit/${id}`;
-    }
-
-    async deleteUser(id) {
-        await fetch(`../api/users/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${this.state.token}`
-            }
-        })
-            .then(response => {
-                if (!response.ok) {
-                    return response.text().then(error => { throw new Error(error) });
-                }
-            })
-            .catch(error => alert(error.message));
+    handleUsers = (users) => {
+        this.setState({users: users});
     }
 
     showUserInfo() {
@@ -66,50 +48,21 @@ export class Users extends Component {
                         <th className="text-center">Id</th>
                         <th className="text-center">Username</th>
                         <th className="text-center">Role</th>
+                        {this.state.role === '0' &&
+                            <th className="text-center">Actions</th>
+                        }
                     </tr>
                 </thead>
                 <tbody>
                 {
                     this.state.users.map(user => {
-                        return <UserRow key={user.id}
+                        return <UserRow onGetUsers={this.handleUsers}
+                                        key={user.id}
                                         id={user.id}
                                         username={user.username}
-                                        role={user.role} />
-                    })
-                }
-                </tbody>
-            </table>
-        </div>;
-    }
-
-    showUserInfoForAdmin() {
-        return <div>
-            <table className="table table-bordered">
-                <thead>
-                    <tr>
-                        <th className="text-center">Id</th>
-                        <th className="text-center">Username</th>
-                        <th className="text-center">Role</th>
-                        <th className="text-center">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    this.state.users.map(user => {
-                        return <tr key={user.id}>
-                            <td>{ user.id }</td>
-                            <td>{ user.username }</td>
-                            <td>{ user.role }</td>
-                            <td className="text-center d-flex justify-content-around">
-                                <button onClick={() => this.updateUser(user.id)} className="btn btn-sm btn-info text-white">Edit</button>
-                                <button onClick={
-                                    async () => { 
-                                        await this.deleteUser(user.id); 
-                                        await this.getUsers(); 
-                                    }
-                                } className="btn btn-sm btn-danger">Delete</button>
-                            </td>
-                        </tr>;
+                                        role={user.role}
+                                        calledFromAdmin={this.state.role === '0'}
+                                        getUsers={this.getUsers} />
                     })
                 }
                 </tbody>
@@ -118,10 +71,6 @@ export class Users extends Component {
     }
 
     render() {
-        if (this.state.role === "0") {
-            return this.showUserInfoForAdmin();
-        } else {
-            return this.showUserInfo();
-        }
+        return this.showUserInfo();
     }
 }
