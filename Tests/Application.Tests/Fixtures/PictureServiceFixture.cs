e25cd9f5860c27efe70ef1df7616890ac2c00537
Vitalik Knysh <knish.vital@gmail.com>
2022-09-10 12:39:27 +0300

    Created unit-tests for PictureService

---
+++ Tests/Application.Tests/Fixtures/PictureServiceFixture.cs
@@ -0,0 +1,34 @@
+﻿using Application.Services;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core.Interfaces.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Application.Tests.Fixtures
+{
+    public class PictureServiceFixture
+    {
+        public PictureServiceFixture()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            MockPictureRepository = fixture.Freeze<Mock<IPictureRepository>>();
+            MockLogger = fixture.Freeze<Mock<ILogger<PictureService>>>();
+
+            MockPictureService = new(
+                MockPictureRepository.Object,
+                MockLogger.Object);
+
+            ValidPath = "../../../../../Application/Assets/Images/default_profile_pic.jpg";
+            InvalidPath = null;
+        }
+
+        public PictureService MockPictureService { get; }
+        public Mock<IPictureRepository> MockPictureRepository { get; }
+        public Mock<ILogger<PictureService>> MockLogger { get; }
+
+        public string ValidPath { get; }
+        public string? InvalidPath { get; }
+    }
+}
