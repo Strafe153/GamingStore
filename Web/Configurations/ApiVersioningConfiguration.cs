0ee7fa5793be906112e6aca9c7975a9ddd7efc59
Vitalik Knysh <knish.vital@gmail.com>
2023-07-16 14:05:15 +0300

    Removed commented code from the api versioning configuration

--- Web/Configurations/ApiVersioningConfiguration.cs
+++ Web/Configurations/ApiVersioningConfiguration.cs
@@ -13,10 +13,6 @@ public static class ApiVersioningConfiguration
                 options.DefaultApiVersion = new ApiVersion(1, 0);
                 options.AssumeDefaultVersionWhenUnspecified = true;
                 options.ReportApiVersions = true;
-                //options.ApiVersionReader = ApiVersionReader.Combine(
-                //    new QueryStringApiVersionReader(),
-                //    new HeaderApiVersionReader(),
-                //    new MediaTypeApiVersionReader());
                 options.ApiVersionReader = new UrlSegmentApiVersionReader();
             })
             .AddVersionedApiExplorer(options =>
