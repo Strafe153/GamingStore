ad9c13d8f3fe87a0447391a2a9fc8eaba37aeb34
Vitalik Knysh <knish.vital@gmail.com>
2024-04-11 20:49:29 +0300

    Removed unnecessary async await from the IdentityServerClient

--- Application/HttpClients/IdentityServerClient.cs
+++ Application/HttpClients/IdentityServerClient.cs
@@ -5,15 +5,15 @@ namespace Application.HttpClients;
 
 public class IdentityServerClient
 {
-    private readonly HttpClient _httpClient;
+	private readonly HttpClient _httpClient;
 
-    public IdentityServerClient(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
+	public IdentityServerClient(HttpClient httpClient)
+	{
+		_httpClient = httpClient;
+	}
 
-    public async Task CheckIdentityServerHealth() =>
-        await _httpClient.PostAsync(
-            $"{_httpClient.BaseAddress}/connect/token",
-            JsonContent.Create(JsonConvert.SerializeObject("{}")));
+	public Task CheckIdentityServerHealth() =>
+		_httpClient.PostAsync(
+			$"{_httpClient.BaseAddress}/connect/token",
+			JsonContent.Create(JsonConvert.SerializeObject("{}")));
 }
